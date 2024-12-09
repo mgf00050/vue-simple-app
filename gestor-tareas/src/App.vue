59bb6d44411be90bb22bfdd59ba43e47a4e2472d
Miguel Gómez Frieyro <mgf00050@red.ujaen.es>
2024-12-09 18:11:20 +0100

    Implementar App.vue

---
+++ gestor-tareas/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>Gestor de Tareas</h1>
+    <AddTask @add-task="addTask" />
+    <TaskList :tasks="tasks" @delete-task="deleteTask" />
+  </div>
+</template>
+
+<script>
+import AddTask from './components/AddTask.vue';
+import TaskList from './components/TaskList.vue';
+
+export default {
+  components: {
+    AddTask,
+    TaskList,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask(newTask) {
+      this.tasks.push(newTask);
+    },
+    deleteTask(taskId) {
+      this.tasks = this.tasks.filter(task => task.id !== taskId);
+    },
+  },
+};
+</script>
