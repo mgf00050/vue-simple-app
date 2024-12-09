1ea4cb697468294a3b0078ca452daec5cf8ee2ed
Miguel Gómez Frieyro <mgf00050@red.ujaen.es>
2024-12-09 18:13:03 +0100

    Implementar funcion  para mostrar tarea individual

--- gestor-tareas/src/components/TaskItem.vue
+++ gestor-tareas/src/components/TaskItem.vue
@@ -1,11 +1,29 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <div class="task-item">
+    <span :class="{ completed: task.completed }">{{ task.name }}</span>
+    <button @click="$emit('delete-task', task.id)">Eliminar</button>
+  </div>
 </template>
 
-<style scoped>
+<script>
+export default {
+  props: {
+    task: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
 
+<style scoped>
+.task-item {
+  display: flex;
+  justify-content: space-between;
+  margin: 5px 0;
+}
+.completed {
+  text-decoration: line-through;
+  color: gray;
+}
 </style>
